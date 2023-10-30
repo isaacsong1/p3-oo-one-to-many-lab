a5f1bd9149b74f392bcd0050688c4c1220617077
Isaac Song <isaacsongs10@gmail.com>
2023-10-29 19:05:52 -0700

    remove lines

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,61 @@
+# import ipdb; ipdb.set_trace()
+
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=""):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        type(self).all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise Exception            
+        else:
+            self._name = new_name
+        
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, new_pet_type):
+        if new_pet_type in type(self).PET_TYPES:
+            self._pet_type = new_pet_type
+        else:
+            raise Exception
+        
+    @property
+    def owner(self):
+        return self._owner
+    
+    @owner.setter
+    def owner(self, new_owner):
+        if not (isinstance(owner, Owner) or not owner):
+            raise TypeError("Object is not of type Owner")
+        else:
+            self._owner = new_owner
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner is self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Input but be of type Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
